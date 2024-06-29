b92889f8df6c474522ac7fc5ccb31d5ff5402b88
Ahmed EL Abid <ahmedelabide@gmail.com>
2024-06-29 12:52:20 -0700

    mandatory

--- tests/test_models/test_amenity.py
+++ tests/test_models/test_amenity.py
@@ -1,19 +1,23 @@
 #!/usr/bin/python3
 """ """
-from tests.test_models.test_base_model import test_basemodel
+import os
+
+from tests.test_models.test_base_model import TestBasemodel
 from models.amenity import Amenity
 
 
-class test_Amenity(test_basemodel):
-    """ """
-
+class TestAmenity(TestBasemodel):
+    """Represents the aminity test models."""
     def __init__(self, *args, **kwargs):
-        """ """
+        """Begins test class."""
         super().__init__(*args, **kwargs)
         self.name = "Amenity"
         self.value = Amenity
 
     def test_name2(self):
-        """ """
+        """Tests the typed name."""
         new = self.value()
-        self.assertEqual(type(new.name), str)
+        self.assertEqual(
+            type(new.name),
+            str if os.getenv('HBNB_TYPE_STORAGE') != 'db' else type(None)
+        )
--- tests/test_models/test_user.py
+++ tests/test_models/test_user.py
@@ -1,34 +1,48 @@
 #!/usr/bin/python3
 """ """
-from tests.test_models.test_base_model import test_basemodel
+import os
+from sqlalchemy import Column
+
+from tests.test_models.test_base_model import TestBasemodel
 from models.user import User
 
 
-class test_User(test_basemodel):
-    """ """
-
+class TestUser(TestBasemodel):
+    """Represents tests for User model."""
     def __init__(self, *args, **kwargs):
-        """ """
+        """Initializes test class."""
         super().__init__(*args, **kwargs)
         self.name = "User"
         self.value = User
 
     def test_first_name(self):
-        """ """
+        """Tests type of first_name."""
         new = self.value()
-        self.assertEqual(type(new.first_name), str)
+        self.assertEqual(
+            type(new.first_name),
+            str if os.getenv('HBNB_TYPE_STORAGE') != 'db' else type(None)
+        )
 
     def test_last_name(self):
-        """ """
+        """Tests type of last_name."""
         new = self.value()
-        self.assertEqual(type(new.last_name), str)
+        self.assertEqual(
+            type(new.last_name),
+            str if os.getenv('HBNB_TYPE_STORAGE') != 'db' else type(None)
+        )
 
     def test_email(self):
-        """ """
+        """Tests type of email."""
         new = self.value()
-        self.assertEqual(type(new.email), str)
+        self.assertEqual(
+            type(new.email),
+            str if os.getenv('HBNB_TYPE_STORAGE') != 'db' else type(None)
+        )
 
     def test_password(self):
-        """ """
+        """Tests type of password."""
         new = self.value()
-        self.assertEqual(type(new.password), str)
+        self.assertEqual(
+            type(new.password),
+            str if os.getenv('HBNB_TYPE_STORAGE') != 'db' else type(None)
+        )
