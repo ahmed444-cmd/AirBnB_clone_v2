c2ea8980e224c37bcbfc7d3dbf8f07234599c373
Ahmed EL Abid <ahmedelabide@gmail.com>
2024-06-29 12:26:32 -0700

    mandatory

---
+++ tests/__init__.py
@@ -0,0 +1,66 @@
+#!/usr/bin/python3
+"""Tests for AirBnb clone modules.
+"""
+import os
+from typing import TextIO
+from models.engine.file_storage import FileStorage
+
+
+def clear_stream(stream: TextIO):
+    """Clears the contents of a stream
+
+    Args:
+        stream (TextIO): The clearing stream.
+    """
+    if stream.seekable():
+        stream.seek(0)
+        stream.truncate(0)
+
+
+def delete_file(file_path: str):
+    """Removes a file.
+    Args:
+        file_path (str): The file name to remove.
+    """
+    if os.path.isfile(file_path):
+        os.unlink(file_path)
+
+
+def reset_store(store: FileStorage, file_path='file.json'):
+    """Resets the items in the store.
+    Args:
+        store (FileStorage): The reseting FileStorage.
+        file_path (str): store's file's path.
+    """
+    with open(file_path, mode='w') as file:
+        file.write('{}')
+    if store is not None:
+        store.reload()
+
+
+def read_text_file(file_name):
+    """Reads the contents of a file.
+
+    Args:
+        file_name (str): The file name to read.
+
+    Returns:
+        str: The file content.
+    """
+    lines = []
+    if os.path.isfile(file_name):
+        with open(file_name, mode='r') as file:
+            for line in file.readlines():
+                lines.append(line)
+    return ''.join(lines)
+
+
+def write_text_file(file_name, text):
+    """Writes a text to a file.
+
+    Args:
+        file_name (str): The file name to write in.
+        text (str): Content of the file.
+    """
+    with open(file_name, mode='w') as file:
+        file.write(text)
