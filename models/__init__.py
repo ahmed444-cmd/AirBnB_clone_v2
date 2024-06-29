aca9b5403258962d755254b3e4a8fdaa9de85843
Ahmed EL Abid <ahmedelabide@gmail.com>
2024-06-29 13:20:21 -0700

    mandatory

--- models/__init__.py
+++ models/__init__.py
@@ -1,7 +1,12 @@
 #!/usr/bin/python3
 """This module instantiates an object of class FileStorage"""
+import os
+
+from models.engine.db_storage import DBStorage
 from models.engine.file_storage import FileStorage
 
+storage = DBStorage() if os.getenv(
+    'HBNB_TYPE_STORAGE') == 'db' else FileStorage()
+"""A single instance of FileStorage/DBStorage shared by all models."""
 
-storage = FileStorage()
 storage.reload()
