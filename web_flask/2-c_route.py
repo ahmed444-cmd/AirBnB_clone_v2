4d1ecb11898a760813fbd6a6f78281eccc3c059a
Ahmed EL Abid <ahmedelabide@gmail.com>
2024-07-29 06:20:56 -0700

    Mandatory

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+'''A Flask web application.
+'''
+from flask import Flask
+
+
+app = Flask(__name__)
+'''The instance of Flask web app'''
+app.url_map.strict_slashes = False
+
+
+@app.route('/')
+def index():
+    '''Home'''
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb')
+def hbnb():
+    '''The hbnb page.'''
+    return 'HBNB'
+
+
+@app.route('/c/<text>')
+def c_page(text):
+    '''The c page.'''
+    return 'C {}'.format(text.replace('_', ' '))
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port='5000')
