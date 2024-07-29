aa851d95e9936bea90ee41d9978bed424c204c3f
Ahmed EL Abid <ahmedelabide@gmail.com>
2024-07-29 07:01:06 -0700

    Mandatory

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -1,35 +1,26 @@
 #!/usr/bin/python3
-'''A simple Flask web app.
-'''
+""" Starts a Flask app """
 from flask import Flask, render_template
-
 from models import storage
 from models.state import State
 
-
 app = Flask(__name__)
-'''The Flask app inst.'''
 app.url_map.strict_slashes = False
 
 
-@app.route('/cities_by_states')
-def cities_by_states():
-    '''The cities page.'''
-    all_states = list(storage.all(State).values())
-    all_states.sort(key=lambda x: x.name)
-    for state in all_states:
-        state.cities.sort(key=lambda x: x.name)
-    ctxt = {
-        'states': all_states
-    }
-    return render_template('8-cities_by_states.html', **ctxt)
-
-
 @app.teardown_appcontext
-def flask_teardown(exc):
-    '''The Flask app/request.'''
+def dispose(exception):
+    """ Delet current session """
     storage.close()
 
 
+@app.route('/states_list')
+def states():
+    """ Display list of all the states """
+    states = storage.all(State)
+    states_list = list(states.values())
+    return render_template('7-states_list.html', states=states_list)
+
+
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port='5000')
+    app.run(host='0.0.0.0', port=5000)
