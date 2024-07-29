d678766960564cd9bd3a97a17f13cd3cfb41306e
Ahmed EL Abid <ahmedelabide@gmail.com>
2024-07-29 06:18:59 -0700

    Mandatory

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -1,22 +1,25 @@
 #!/usr/bin/python3
-'''An example of a simple web application using Flask.
+'''A straightforward web app built with Flask.
 '''
 from flask import Flask
 
+
 app = Flask(__name__)
-'''Creating an instance of the Flask application.'''
+'''The instance of Flask app.'''
 app.url_map.strict_slashes = False
+
 
 @app.route('/')
 def index():
-    '''Handler for the home route.'''
+    '''Home.'''
     return 'Hello HBNB!'
+
 
 @app.route('/hbnb')
 def hbnb():
-    '''Handler for the /hbnb route.'''
+    '''The hbnb page.'''
     return 'HBNB'
 
+
 if __name__ == '__main__':
-    '''Runs the application on the specified host and port.'''
     app.run(host='0.0.0.0', port='5000')
